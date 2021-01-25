60e3a4b8b6fd99b7a3a90c43b799bdf57bd04933
Curtis H <sitrucrettah1@gmail.com>
2021-01-24 17:29:51 -0800

    Create employee function, Create manager/engineer/intern function, all work as intended

--- Develop/app.js
+++ Develop/app.js
@@ -9,39 +9,60 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Choices = require("inquirer/lib/objects/choices");
 
+let team = [];
+let internCount = [];
+let engineerCount = [];
+let managerCount = [];
+const employ = [
+    {
+        type: "list",
+        message: "Add an Employee: ",
+        name: "employee",
+        choices: ["Manager", "Engineer", "Intern", "Team Complete"]
+    }
+];
 const managerInfo = [
     {
         type: "input",
-        message: "What's the manager's name of your team?",
+        message: "Name: ",
         name: "name"
     },
     {
         type: "input",
-        message: "Your manager's identificaiton number?",
+        message: "Identificaiton number: ",
         name: "id"
     },
     {
         type: "input",
-        message: "Your manager's email?",
+        message: "Email: ",
         name: "email"
     },
     {
         type: "input",
-        message: "Your manager's office number?",
+        message: "Office Number: ",
         name: "officeNumber"
     },
     {
         type: "confirm",
-        message: "Any engineers?",
-        name: "hasEngineers"
+        message: "Any more Managers?",
+        name: "hasMoreManagers"
     },
-    // {
-    //     type: "confirm",
-    //     message: "Any interns?",
-    //     name: "hasInterns"
-    // }
 ];
+// const prompts = [
+//     {
+//         type: "input",
+//         message: "Your engineer's github?",
+//         name: "officeNumber"
+//     },
+//     {
+//         type: "confirm",
+//         message: "Any more?",
+//         name: "hasMoreEngineers",
+//         default: true
+//     },
+// ];
 
 
 
@@ -59,33 +80,129 @@ const managerInfo = [
 //     }
 // })
 
-const collectInputs = async (inputs = []) => {
-    const prompts = [
+const collectEmployees = async () => {
+    const { employee, ...answers } = await inquirer.prompt(employ);
+    // const newInputs = [...inputs];
+    // console.log(employee);
+    // return newInputs;
+    if (employee === "Manager") {
+        return collectManagerInputs();
+    }
+    else if (employee === "Engineer") {
+        return collectEngineerInputs();
+    }
+    else if (employee === "Intern") {
+        return collectInternInputs();
+    }
+    else {
+        managerCount.forEach(manager => {
+            team.push(manager);
+        });
+        engineerCount.forEach(engineer => {
+            team.push(engineer);
+        });
+        internCount.forEach(intern => {
+            team.push(intern);
+        })
+        // team.push(managerCount, engineerCount, internCount);
+        return team;
+    }
+};
+
+const collectManagerInputs = async () => {
+    const { hasMoreManagers, ...answers } = await inquirer.prompt(managerInfo);
+    const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    managerCount.push(newManager);
+    // return collectEmployees(team);
+    // const newInputs = [...inputs, answers];
+    // managerCount++;
+    // if (hasMoreManagers) {
+    //     return collectManagerInputs(newInputs);
+    // }
+    // else if (hasEngineer) {
+    //     return collectEngineerInputs(newInputs);
+    // }
+    // else if (hasIntern) {
+    //     return collectInternInputs(newInputs);
+    // }
+    // else {
+    //     return newInputs;
+    // }
+    // return newInputs;
+    return hasMoreManagers ? collectManagerInputs() : collectEmployees();
+};
+
+const collectEngineerInputs = async () => {
+    managerInfo.pop();
+    managerInfo.pop();
+    managerInfo.push(
         {
             type: "input",
-            message: "Your manager's office number?",
-            name: "officeNumber"
+            message: "Your engineer's github?",
+            name: "github"
         },
         {
             type: "confirm",
-            message: "Any engineers?",
-            name: "hasEngineers",
+            message: "Any more?",
+            name: "hasMoreEngineers",
             default: true
-        },
-    ];
-
-    const { hasEngineers, ...answers } = await inquirer.prompt(prompts);
-    const newInputs = [...inputs, answers];
-    return hasEngineers ? collectInputs(newInputs) : newInputs;
+        });
+    const prompts = managerInfo;
+    // engineerCount++;
+    const { hasMoreEngineers, ...answers } = await inquirer.prompt(prompts);
+    const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    engineerCount.push(newEngineer);
+    // const newInputs = [...inputs, answers];
+    // if (hasMoreEngineers) {
+    //     return collectEngineerInputs(newInputs);
+    // }
+    // else if (hasIntern) {
+    //     return collectInternInputs(newInputs);
+    // }
+    // else {
+    //     return newInputs;
+    // }
+    return hasMoreEngineers ? collectEngineerInputs() : collectEmployees();
 };
 
+const collectInternInputs = async () => {
+    managerInfo.pop();
+    managerInfo.pop();
+    managerInfo.push(
+        {
+            type: "input",
+            message: "Your intern's school?",
+            name: "school"
+        },
+        {
+            type: "confirm",
+            message: "Any more?",
+            name: "hasMoreInterns",
+            default: true
+        });
+    const prompts = managerInfo;
+    // internCount++;
+    const { hasMoreInterns, ...answers } = await inquirer.prompt(prompts);
+    // const newInputs = [...inputs, answers];
+    const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    internCount.push(newIntern);
+    return hasMoreInterns ? collectInternInputs() : collectEmployees();
+}
+
 const main = async () => {
-    const inputs = await collectInputs();
+    const inputs = await collectEmployees();
+    // const inputs = await collectManagerInputs();
     console.log(inputs);
+    // console.log(team);
 };
 
 main();
 
+// let team = await render(inputs);
+//     fs.writeFile(outputPath, team, "utf-8", function (err) {
+//         if (err) console.log("You screwed up", err);
+//     })
+//     console.log("You did it");
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
